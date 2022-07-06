7ed9fd071533328cec8605c1d85d4ea9c94fbcc1
Mustapha <mustapha_dhikrullah@hotmail.com>
2022-07-06 05:12:05 -0700

    0x04. Python - More Data Structures: Set, Dictionary file created

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 100-weight_average.py
+
+def weight_average(my_list=[]):
+    """Return the weighted average of all integers in a list of tuples."""
+    if not isinstance(my_list, list) or len(my_list) == 0:
+        return (0)
+
+    average = 0
+    size = 0
+    for tup in my_list:
+        average += (tup[0] * tup[1])
+        size += tup[1]
+    return (average / size)
