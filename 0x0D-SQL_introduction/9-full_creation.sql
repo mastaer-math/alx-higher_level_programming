91809b0b358d968269e55adfd28f6a2c76de05a6
Mustapha <mustapha_dhikrullah@hotmail.com>
2022-09-06 06:46:06 -0700

    error fixed

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -12,8 +12,8 @@
 --      id = 3, name = “Bob”, score = 14
 --      id = 4, name = “George”, score = 8
 
-CREATE TABLE IF NOT EXISTS second_table (id INT,
-	name VARCHAR(256),
+CREATE TABLE IF NOT EXISTS second_table (id INT, 
+	name VARCHAR(256), 
 	score INT);
 	INSERT INTO second_table (id, name, score)
 	VALUES (1, "John", 10),
