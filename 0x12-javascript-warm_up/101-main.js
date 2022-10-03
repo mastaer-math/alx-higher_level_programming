4e14112c5758d12644bf73d33902d8badbccc58e
Mustapha <mustapha_dhikrullah@hotmail.com>
2022-10-03 08:46:44 -0700

    Javascript warmup

---
+++ 0x12-javascript-warm_up/101-main.js
@@ -0,0 +1,54 @@
+#!/usr/bin/node
+const callMeMoby = require('./101-call_me_moby').callMeMoby;
+callMeMoby(3, function () {
+  console.log('C is fun');
+});
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"101-main.js" [New File]                                                  0,0-1         All
+
